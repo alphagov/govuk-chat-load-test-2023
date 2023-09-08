a967a3750606be1dc0dfee090caa94c875e0b8b4
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-09-08 10:16:32 +0100

    Add chat_id as env var/param - update shell script to take chat_id

--- run.sh
+++ run.sh
@@ -1,15 +1,23 @@
 #! /bin/sh
 
-rm -rf results
-rm jmeter.log
+rm -f jmeter.log
 
-jmeter -JPROTOCOL=${PROTOCOL} \
-       -JHOST=${HOST} \
-       -JROUTE=${ROUTE} \
-       -JUSERNAME=${USERNAME} \
-       -JPASSWORD=${PASSWORD} \
-       -JAUTH_URL=${AUTH_URL} \
-       -JUSERS=${USERS} \
-       -JRAMP_UP=${RAMP_UP} \
-       -JITERATIONS=${ITERATIONS} \
-       -n -t plan.jmx -l results/results.log -e -o results
+if [ -z "$1" ]; then
+  echo "Please enter a chat_id, e.g. sh run.sh 123456789"
+  exit 1
+else
+  chat_id="load_test-${1}-${USERS}-${RAMP_UP}-${ITERATIONS}"
+  rm -rf results-${chat_id}
+
+  jmeter -JPROTOCOL=${PROTOCOL} \
+         -JHOST=${HOST} \
+         -JROUTE=${ROUTE} \
+         -JUSERNAME=${USERNAME} \
+         -JPASSWORD=${PASSWORD} \
+         -JAUTH_URL=${AUTH_URL} \
+         -JUSERS=${USERS} \
+         -JRAMP_UP=${RAMP_UP} \
+         -JITERATIONS=${ITERATIONS} \
+         -JCHAT_ID=${chat_id} \
+         -n -t plan.jmx -l results-${chat_id}/results.log -e -o results-${chat_id}
+fi
