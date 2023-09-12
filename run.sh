74466b0e322539c0f9c96b7aa679295f50fb7989
Sonia Hussain <sonia_ahm@icloud.com>
2023-09-12 17:23:02 +0100

    Add JMETER_PLAN env var

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ]; then
   echo "Please enter a chat_id, e.g. sh run.sh 123456789"
   exit 1
 else
-  chat_id="load_test-${1}-${JMETER_USERS}-${JMETER_RAMP_UP}-${JMETER_ITERATIONS}"
+  chat_id="load_test-${1}-${JMETER_PLAN}-${JMETER_USERS}-${JMETER_RAMP_UP}-${JMETER_ITERATIONS}"
   rm -rf results-${chat_id}
 
   jmeter -JJMETER_PROTOCOL=${JMETER_PROTOCOL} \
@@ -15,6 +15,7 @@ else
          -JJMETER_USERNAME=${JMETER_USERNAME} \
          -JJMETER_PASSWORD=${JMETER_PASSWORD} \
          -JJMETER_AUTH_URL=${JMETER_AUTH_URL} \
+         -JJMETER_PLANL=${JMETER_PLAN} \
          -JJMETER_USERS=${JMETER_USERS} \
          -JJMETER_RAMP_UP=${JMETER_RAMP_UP} \
          -JJMETER_ITERATIONS=${JMETER_ITERATIONS} \
